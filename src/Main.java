b891a29337c43e108b72853d0fc67d607f90d00f
Michal Swierczynski <hajzenberk@gmail.com>
2025-02-04 13:31:43 +0100

    First version of program created

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,18 @@
 public class Main {
+
+    public static void main(String[] args) {
+
+        double bmi;
+        double weight;
+        double height;
+
+        weight = 117;
+        height = 1.88;
+
+        bmi = weight / Math.pow(height, 2D);
+
+        System.out.printf("Your BMI is: %.2f", bmi);
+
+    }
+
 }
