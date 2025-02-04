ca4c076e67eae0f940afe4db5c8174faf9e8522d
Michal Swierczynski <hajzenberk@gmail.com>
2025-02-04 15:15:42 +0100

    Added comments and user input for weight and height

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        //variables
         double bmi;
         double weight;
         double height;
 
-        weight = 117;
-        height = 1.88;
+        //user input
+        System.out.print("\nWrite your weight in [kg]: ");
+        weight = scanner.nextDouble();
+        System.out.print("Write your height in [cm]: ");
+        height = scanner.nextDouble() / 100;
 
+        //bmi equation
         bmi = weight / Math.pow(height, 2D);
 
-        System.out.printf("Your BMI is: %.2f", bmi);
+        System.out.printf("\nYour BMI is: %.2f", bmi);
+
+        //method to close scanner
+        scanner.close();
 
     }
 
